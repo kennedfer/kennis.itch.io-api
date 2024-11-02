4ae21be455936f45e2b14d0137aeff5a98d79f77
kennedfer <kenned14santos@gmail.com>
2024-11-02 09:53:35 -0300

    feat: Add api data filter

--- app.js
+++ app.js
@@ -9,11 +9,31 @@ app.use(cors())
 
 const API_KEY = process.env.ITCH_IO_KEY;
 
+function formatGamesData(games) {
+  const data = games.map(({ title, short_text, published_at, downloads_count, cover_url, views_count, url, p_android }) => {
+    return {
+      title,
+      description: short_text,
+      published_at,
+      downloads_count,
+      cover_url,
+      views_count,
+      url,
+      is_android: p_android
+    }
+  })
+
+  return data;
+}
+
 app.get("/my-games", async (req, res) => {
   try {
     const response = await fetch(`https://itch.io/api/1/${API_KEY}/my-games`);
     const data = await response.json();
-    res.json(data);
+
+    const formattedData = formatGamesData(data.games);
+
+    res.json(formattedData);
   } catch (error) {
     res.status(500).json({ error: "Erro ao obter dados" });
   }
