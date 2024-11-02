f3fb1418fd9cb431f91543ce2e20df04391e5c1a
kennedfer <kenned14santos@gmail.com>
2024-11-02 09:06:51 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+const API_KEY = process.env.ITCH_IO_KEY;
+
+app.get("/my-games", async (req, res) => {
+  try {
+    const response = await fetch(`https://itch.io/api/1/${API_KEY}/my-games`);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao obter dados" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
